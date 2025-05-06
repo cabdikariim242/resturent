45a9ae517663711aa2b1a940ac43ad6932f5df83
cabdikariim242 <cabdikariimmaxamaddaahir@gmail.com>
2025-05-06 13:34:04 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const buttonEl = document.querySelector('.btn');
+
+buttonEl.addEventListener('mouseover', (event) => {
+const x = event.pageX - buttonEl.offsetLeft;
+const y = event.pageY - buttonEl.offsetTop;
+
+
+buttonEl.style.setProperty("--xpos",  x + "px");
+buttonEl.style.setProperty("--ypos",  y + "px");
+
+})
